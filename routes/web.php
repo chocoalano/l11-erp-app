d01dff66e84dc703c66a9ead05bddcb48a2e842f
chocoalano <alangentina95@gmail.com>
2024-06-29 11:02:15 +0700

    update 29062024

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\Web\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/hr');
+    return view('welcome');
 });
 
 Route::get('/format-excel-user-import', [UserController::class, 'downloadFormatExcelImport'])->name('download.user.format.excel');
